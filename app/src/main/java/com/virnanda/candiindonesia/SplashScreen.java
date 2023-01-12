738c5ee20035a1a1ef51bd295a8f4ac507eee1ce
Mayriska26 <mayriskaputri789@gmail.com>
2023-01-12 22:49:01 +0700

    Melengkapi detail alamat pada data candi

--- app/src/main/java/com/virnanda/candiindonesia/SplashScreen.java
+++ app/src/main/java/com/virnanda/candiindonesia/SplashScreen.java
@@ -12,17 +12,22 @@ import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
+    //initialize variable
     private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //menetapkan variabel
         textView = findViewById(R.id.textView);
 
+        //initialize text animation
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.transisi_turun);
         textView.setAnimation(animation);
 
+        //setting tampilan full screen
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(decorView.SYSTEM_UI_FLAG_FULLSCREEN);
 
@@ -30,11 +35,13 @@ public class SplashScreen extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
-
+        //initialize handler
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                //mengalihkan ke activity utama
                 startActivity(new Intent(SplashScreen.this, MainActivity.class ));
+                //menyelesaikan activity
                 finish();
             }
         }, 2000);
