f2688df2dc91e7bfd536235787563c0c3997c124
yulimaurya22 <yulimaurya@gmail.com>
2023-06-14 22:49:13 +0700

    Notifikasi

--- app/src/main/java/com/virnanda/candiindonesia/Notifikasi.java
+++ app/src/main/java/com/virnanda/candiindonesia/Notifikasi.java
@@ -1,7 +1,14 @@
 package com.virnanda.candiindonesia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class Notifikasi extends AppCompatActivity {
@@ -11,4 +18,26 @@ public class Notifikasi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifikasi);
     }
-}
+    private void createNotif(){    {
+        String id = "Gambar_Candi_Baru";
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.0)
+       {
+           NotificationChannel channel =manager.getNotificationChannel(id);
+           if(channel == null)
+           {
+               channel = new NotificationChannel(id, "candi terbaru", NotificationManager.IMPORTANCE_HIGH);
+               channel.setDescription("[Channel description]");
+               channel.enableVibration(true);
+               channel.setVibrationPattern(new long []{100,1000,200,300});
+               channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+               manager.createNotificationChannel(channel);
+           }
+       }
+        Intent notificationIntent = new Intent(this,Notifikasi.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent contentIntent =PendingIntent.getActivity(this,0,notificationIntent,0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,id)
+                .setSmallIcon(R.drawable.icon)
+
+    }
